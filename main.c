a0c1bf11f392c644ca9ecb9ccde7253a29769298
kyrychenkoV <keryvn@gmail.com>
2016-10-10 15:28:21 +0300

    funkc

--- main.c
+++ main.c
@@ -1,60 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
-FILE * openFile(char *mode);
-void recordTofile(int **pointer,int sum0,int sum1,int count);
-int main(int argc, char *argv[])
+#include "funkc.h"
+#include "funkc.c"
+
+
+typedef struct Array{
+    int intnumber[8];
+    int floatnumber[8];
+} Array;
+
+Array scanInFileToStruct(Array pointer);
+int count;
+int main()
 {
-    FILE * myfile ;
-    myfile=openFile("w");
+    Array pointer;
+    int sum;
+    int sum2;
+    char *teststring="10.10 20.12 13.14 40.50 50.16 70.21 80.32 90.18";
+    addNewFiles(teststring);
+    pointer=scanInFileToStruct(pointer);
+    sum=functionSumElementArray(pointer.intnumber,count);
 
-    char *teststring="10.1 20.3 13.4 40.5 50.5 70.3 80.4 90.2";
-    fprintf(myfile,"%s",teststring);
-    fclose(myfile);
+    sum2=functionSumElementArray(pointer.floatnumber,count);
+    recordSumToFile(sum);
+    recordSumToFile(sum2);
+    return 0;
+}
 
-    // add dynamic memory for 0 array pointer
-    int **pointer=(int **)malloc(sizeof(int*));
-    pointer[0]=(int *)malloc(2*sizeof(int));
-
-    myfile=openFile("r");
+Array scanInFileToStruct(Array pointer){
+    FILE * myfile;
     int i=0;
-    int count=0;
-    //1)scan from the file to array
-    while((fscanf(myfile, "%d.%d", &pointer[i][0], &pointer[i][1]))!=EOF){
+    myfile=fopen(fileName(),"r");
+    while((fscanf(myfile, "%d.%d", &pointer.intnumber[i],&pointer.floatnumber[i]))!=EOF){
         i++;
-        // add dynamic memory for i+1 pointers
-        pointer=(int **)realloc(pointer,(i+1)*sizeof(int*));
-        pointer[i]=(int *)malloc((2)*sizeof(int));
         count++;
     }
     fclose(myfile);
-    //2 Finding the sum of the elements
-    int sum0=0;
-    int sum1=0;
-    for (i=0;i<count;i++){
-        sum0+= *(*(pointer+i)+0);
-        sum1+=*(*(pointer+i)+1);
-        printf(" %d %d\n",*(*(pointer+i)+0),*(*(pointer+i)+1));
-    }
-    printf("\n %d %d",sum0,sum1);
-    //3 record ineger numbers and sum to file
-    recordTofile(pointer,sum0,sum1,count);
+    return pointer;
+}
 
-    return 0;
-}
-FILE *openFile(char *mode){
-    FILE *myfile;
-    myfile=fopen("doublenumbers.txt",mode);
-    return myfile;
-}
-void recordTofile(int **pointer,int sum0,int sum1,int count){
-    FILE *myfile;
-    int i;
-    myfile=openFile("a");
-    fprintf(myfile,"%s","\nInteger numbers:");
-    for(i=0;i<count;i++){
-    fprintf(myfile,"% d",pointer[i][0]);
-    }
-    fprintf(myfile,"\nSumm intgerr=%d sum float=%d",sum0,sum1);
-    fclose(myfile);
-}
-//void findSumElements(int**pointer){}
